2c9cf326e4ef86f82baf7fee70facc726fbce30e
Ma Ngoc Hung Thang <mathangspk@gmail.com>
2020-12-02 05:36:49 +0700

    fix be show wo user tool in use

--- routes/api/tools.js
+++ routes/api/tools.js
@@ -8,80 +8,74 @@ const Tool = require('../../models/Tool');
 //@route GeT api/tools
 //@desc Get all tools
 router.get('/', verify, (req, res) => {
-    // Tool.aggregate([
-    //     // { $project : {
-    //     //     _id : 1,
-    //     //     userId: 1
-    //     //   }
-    //     // },
-    //     { $lookup : {
-    //         "from" : "orders",
-    //         "localField" : "_id",
-    //         "foreignField" : "toolId",
-    //         "as" : "woInfo"      
-    //       }
-    //     },
-    //     {
-    //         $unwind: "$woInfo"
-    //     },
-    //     {
-    //         $lookup: {
-    //             "from": "users",
-    //             "localField": "woInfo.userId",
-    //             "foreignField": "_id",
-    //             "as": "woInfo.detail"
-    //         }
-    //     }
-    //   ]).then(tools => res.json(tools));
-        Tool.aggregate([
-            {
-                $lookup: {
-                    from: "orders",
-                    let: { id: "$_id" },
-                    pipeline: [
-                        { $match: { $expr: { $in: ["$$id", "$toolId"] } } },
-                        {
-                            $lookup: {
-                                from: "users",
-                                let: { id: "$userId" },
-                                pipeline: [
-                                    { $match: { $expr: { $eq: ["$$id", "$_id"] } } },
-                                    { $project : {
-                                        _id : 1,
+    Tool.aggregate([
+        {
+            $lookup: {
+                from: "orders",
+                let: { id: "$_id" },
+                pipeline: [
+                    { $match: { $expr: { $in: ["$$id", "$toolId"] } } },
+                    {
+                        $lookup: {
+                            from: "users",
+                            let: { id: "$userId" },
+                            pipeline: [
+                                { $match: { $expr: { $eq: ["$$id", "$_id"] } } },
+                                {
+                                    $project: {
+                                        _id: 1,
                                         name: 1
-                                      }
                                     }
-                                ],
-                                as: "userInfo"
-                            }
-                        },
-                        { $unwind: "$userInfo" }
-                    ],
-                    as: "woInfo"
-                }
+                                }
+                            ],
+                            as: "userInfo"
+                        }
+                    },
+                    { $unwind: "$userInfo" }
+                ],
+                as: "woInfo"
             }
-        ]).then(tools => res.json(tools));
-    });
-
-    // Tool.find()
-    //     .sort({ date: -1 })
-    //     .then(tools => res.json(tools));
+        }
+    ]).sort({ date: -1 }).then(tools => res.json(tools));
+});
 //@find name
 router.get('/search', verify, (req, res) => {
-    console.log(req.query)
-    var name = req.query.name || '';
-    var manufacturer = req.query.manufacturer || '';
-    var type = req.query.type || '';
-    var status = req.query.status || [1,2,3,4];
-   
-    Tool.find({
-        name: { '$regex': name },
-        manufacturer: { '$regex': manufacturer },
-        type: { '$regex': type },
-        status: { '$in': status }
+    let aggregate = [
+        { $match: { name: { $regex: req.query.name || '' } } },
+        { $match: { manufacturer: { $regex: req.query.manufacturer || '' } } },
+        { $match: { type: { $regex: req.query.type || '' } } }
+    ]
+    if (req.query.status && req.query.status !== 'all') {
+        aggregate.push({ $match: { status: { $eq: parseInt(req.query.status) } } })
+    }
+    aggregate.push({
+        $lookup: {
+            from: "orders",
+            let: { id: "$_id" },
+            pipeline: [
+                { $match: { $expr: { $in: ["$$id", "$toolId"] } } },
+                {
+                    $lookup: {
+                        from: "users",
+                        let: { id: "$userId" },
+                        pipeline: [
+                            { $match: { $expr: { $eq: ["$$id", "$_id"] } } },
+                            {
+                                $project: {
+                                    _id: 1,
+                                    name: 1
+                                }
+                            }
+                        ],
+                        as: "userInfo"
+                    }
+                },
+                { $unwind: "$userInfo" }
+            ],
+            as: "woInfo"
+        }
     })
-        .sort({ date: -1 })
-        .then(tools => res.status(200).json(tools));
+    Tool.aggregate(aggregate).sort({ date: -1 }).then(tools => res.status(200).json(tools));
 });
 //@skip limit tool
 router.get('/skip', verify, (req, res) => {
@@ -93,15 +87,15 @@ router.get('/skip', verify, (req, res) => {
 //@route POST api/tools
 //@desc Create an tools
 //@access Public
-router.post('/', verify,  async (req, res) => {
+router.post('/', verify, async (req, res) => {
     //let validate the data before we a user
     var reqStatus = req.body.status;
     console.log(req.body)
-    function getStatus(reqStatus){
-        return (reqStatus == "0" ? false:true)
+    function getStatus(reqStatus) {
+        return (reqStatus == "0" ? false : true)
     }
     console.log(getStatus(reqStatus))
-   // const { error } = toolValidation(req.body);
+    // const { error } = toolValidation(req.body);
     //console.log(error);
     //if (error) return res.status(400).json(error.details[0].message);
 
