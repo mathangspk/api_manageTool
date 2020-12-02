789778a8915ee58dd25d4041dc0d455c9c8f8cff
Huannt <huannt@bestarion.com>
2020-12-02 11:26:31 +0700

    Change id

--- routes/user.js
+++ routes/user.js
@@ -88,7 +88,7 @@ router.post('/login', async (req, res) => {
   res.status(200).json({
     token,
     user: {
-      id: user._id,
+      _id: user._id,
       name: user.name,
       email: user.email,
       phone: user.phone,
