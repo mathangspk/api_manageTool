4a2565b44b0f088215d8a46ccc3933ed8e37a9f6
mathangspk <mathangspk@gmail.com>
2021-05-17 09:09:27 +0700

    update link database

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     // mongoURI : 'mongodb://admin:123456@113.161.220.166/manageYenSaoChamPa'
     //mongoURI : 'mongodb://champaUser:champaUser@113.161.220.166:27017/manageYenSaoChamPa'
-   mongoURI : 'mongodb://localhost:27017/manageTool'
+   //mongoURI : 'mongodb://localhost:27017/manageTool'
     //mongoURI:'mongodb://manage:!manage@128.199.82.173:27017/manageTool'
-    //mongoURI:'mongodb://user:123456@128.199.82.173:27017/manageToolTest'
+    mongoURI:'mongodb://user:123456@128.199.82.173:27017/manageToolTest'
 }
